554d637edba9784276552f14ade67bfd73ef91e5
unknown <zyf199966@126.com>
2020-04-07 11:54:25 +0800

    first commit

---
+++ AutoBackup.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+# 自动增量备份插件（仅限Linux，在Ubuntu18.04通过测试）
+# 定时运行：
+# rsync -a --delete back-up/auto/ back-up/auto-last
+# rsync -a --delete server/ back-up/auto
+# author: Sciroccogti
+
+
+import time
+import os
+import re
+import platform
+import subprocess
+from utils import config, constant
+
+interval = 1  # hours between backups
+path = ''
+work_dir = ''
+firsttime = False
+
+def TimeStampToTime(timestamp: float) -> str:
+    "convert timestamp to time (str)"
+    timeStruct = time.localtime(timestamp)
+    return time.strftime('%Y-%m-%d %H:%M:%S', timeStruct)
+
+
+def get_FileAccessTime(filePath: str) -> float:
+    "get the last accessed time in timestamp (sec)"
+    # filePath = unicode(filePath, 'utf8')
+    try:
+        t = os.path.getatime(filePath)
+    except:
+        t = -1
+    return t
+
+
+def on_load(server, old_module):
+    global path, work_dir, firsttime
+    pluginconfig = config.Config(constant.CONFIG_FILE)
+    pluginconfig.read_config()
+    work_dir = pluginconfig['working_directory']
+    path = os.getcwd()  # MCDReforged/
+    try:
+        os.listdir(path + '/back-up')
+    except:
+        server.logger.info(
+            'Directory \'back-up\' not found, trying to create one...')
+        os.mkdir(path + '/back-up')
+    try:
+        os.listdir(path + '/back-up/auto')
+    except:
+        server.logger.info(
+            'Directory \'back-up/auto\' not found, trying to create one...')
+        os.mkdir(path + '/back-up/auto')
+        firsttime = True
+
+
+def on_info(server, info):
+    global path, interval
+    if re.match('!!autobk', info.content) != None:
+        if info.is_player:
+            if info.content == '!!autobk query':
+                text = '§7Last backup is at\n'
+                lasttime = get_FileAccessTime(path + '/back-up/auto')
+                text += TimeStampToTime(lasttime)
+                text += '\ninterval is %d hour' % interval
+                server.tell(info.player, text)
+            elif info.content.startswith('!!autobk set'):
+                try:
+                    new_interval = int(
+                        re.match(r'!!autobk set (\S*)', info.content).groups()[0])
+                except:
+                    text = '§cPlease enter an interger!'
+                    server.tell(info.player, text)
+                else:
+                    interval = new_interval
+                    if interval == 0:
+                        text = 'autobk is turned OFF'
+                        server.say(text)
+                    else:
+                        text = 'autobk interval is set to %d' % interval
+                        server.say(text)
+            else:
+                text = '§7!!autobk§r: Show this message\n'
+                text += '§7!!autobk query§r: Query the time of the last backup and the interval'
+                text += '§7!!autobk set [hour]§r: Set the interval between backups, 0 to turn it off\n'
+                server.tell(info.player, text)
+
+
+def on_player_left(server, player):
+    global firsttime, interval
+    lasttime = get_FileAccessTime(path + '/back-up/auto')
+    if firsttime or (interval > 0 and time.time() - lasttime >= 60 * 60 * interval):
+        firsttime = False
+        if platform.system() == 'Windows':
+            text = '§cWindows is not supported by autobk yet!'
+            server.say(text)
+        else:
+            try:
+                err = subprocess.call(['rsync', '-a', '--delete', 'back-up/auto/', 'back-up/auto-last'])
+            except:
+                text = '§cError during autobk:' + err
+                server.say(text)
+            else:
+                try:
+                    err = subprocess.call(['rsync', '-a', '--delete', work_dir + '/', 'back-up/auto'])
+                except Exception as err:
+                    text = '§cError during autobk:' + str(err)
+                    server.say(text)
+                else:
+                    text = 'autobked successfully!'
+                    server.logger.info(text)
+                    server.say(text)
---
+++ MCDP.py
@@ -0,0 +1,133 @@
+# -*- coding: utf-8 -*-
+
+import os
+import re
+import shutil
+from git import Repo
+
+path = ''
+repo = None
+pluginlist = []
+
+
+def copy_plugin(plugin: str) -> bool:
+    "Copy a plugin and its folder. Return True if succeeded, otherwise return False"
+    if not plugin.endswith('.py'):
+        return False
+    try:
+        shutil.copy(path + plugin, path + '../')
+    except:
+        return False
+    try:  # copy folder
+        os.listdir(path + plugin[0:-3])
+    except:
+        return True
+    else:
+        try:  # remove existing folder in plugins/
+            os.listdir(path + '../' + plugin[0:-3])
+        except:
+            pass
+        else:
+            shutil.rmtree(path + '../' + plugin[0:-3])
+        shutil.copytree(path + plugin[0:-3], path + '../' + plugin[0:-3])
+        return True
+
+
+def remove_plugin(plugin: str) -> bool:
+    "Remove a plugin and its folder. Return True if succeeded, otherwise return False"
+    if not plugin.endswith('.py'):
+        return False
+    try:
+        os.remove(path + '../' + plugin)
+    except:
+        return False
+    try:  # remove folder
+        shutil.rmtree(path + '../' + plugin[0:-3])
+    except:
+        pass
+    return True
+
+
+def on_load(server, old_module):
+    global path, repo
+    path = os.getcwd()  # MCDReforged/
+    path += '/plugins/MCDP/'
+    try:
+        os.listdir(path)
+    except:
+        server.logger.info(
+            'Directory \'plugins/MCDP\' not found, tring to create one...')
+        os.mkdir(path)
+    try:
+        repo = Repo(path)
+    except:
+        server.logger.info(
+            'Repo \'plugins/MCDP\' is bare, tring to clone one...')
+        repo.clone_from('https://github.com/Dark-Night-Base/MCDP.git', path)
+    else:
+        repo.remote().pull()
+
+
+def on_info(server, info):
+    global path, repo, pluginlist
+    if re.match('!!MCDP', info.content) != None:
+        if info.content == '!!MCDP list':
+            pluginlist = os.listdir(path)
+            text = 'Listing...\n'
+            for plugin in pluginlist:
+                if plugin.endswith('.py'):
+                    text += '§2' + plugin + '\n'
+            server.reply(info, text)
+        elif info.content.startswith('!!MCDP install'):
+            try:
+                plugin = re.match(r'!!MCDP install (\S*)',
+                                  info.content).groups()[0]
+            except:
+                server.reply(info, '§cPlease specify plugin!')
+            else:
+                if plugin in pluginlist:
+                    if copy_plugin(plugin):
+                        text = '§aPlugin %s installed successfully!' % plugin
+                    else:
+                        text = '§cPlugin %s installed failed!\n' % plugin
+                        text += '§cConsider add \'.py\' behind the name?'
+                else:
+                    text = '§cPlugin %s not found!' % plugin
+                server.reply(info, text)
+        elif info.content.startswith('!!MCDP remove'):
+            try:
+                plugin = re.match(r'!!MCDP remove (\S*)',
+                                  info.content).groups()[0]
+            except:
+                server.reply(info, '§cPlease specify plugin!')
+            else:
+                if plugin in pluginlist:
+                    if remove_plugin(plugin):
+                        text = '§aPlugin %s removed successfully!' % plugin
+                    else:
+                        text = '§cPlugin %s removed failed!\n' % plugin
+                        text += '§cConsider add \'.py\' behind the name?'
+                else:
+                    text = '§cPlugin %s not found!' % plugin
+                server.reply(info, text)
+
+        elif info.content == '!!MCDP update':
+            fetchinfo = repo.remote().pull()[0]
+            committime = fetchinfo.commit.authored_datetime.strftime(
+                '%b %d %H:%M')
+            server.reply(info, '§7Updated. Last commit at' +
+                         committime + fetchinfo.commit.message)
+        elif info.content == '!!MCDP upgrade':
+            pluginlist = os.listdir(path)
+            for plugin in pluginlist:
+                if plugin.endswith('.py'):
+                    copy_plugin(plugin)
+            server.say('§7Plugins upgraded.')
+        else:
+            text = '§7!!MCDP§r: Show this message\n'
+            text += '§7!!MCDP list§r: List the plugins\n'
+            text += '§7!!MCDP install [plugin]§r: Install or upgrade plugins\n'
+            text += '§7!!MCDP remove [plugin]§r: Remove the plugins\n'
+            text += '§7!!MCDP update§r: Update list of available plugins\n'
+            text += '§7!!MCDP upgrade§r: Upgrade the plugins'
+            server.tell(info.player, text)
